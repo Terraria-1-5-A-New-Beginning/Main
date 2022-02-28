cf28f408232d5725f3269d7eac1882b53f49ccde
ikee1 <89046438+ikee1@users.noreply.github.com>
2022-02-28 17:08:59 +0000

    Add files via upload

---
+++ ANB/ANB.cs
@@ -0,0 +1,8 @@
+using Terraria.ModLoader;
+
+namespace ANB
+{
+	public class ANB : Mod
+	{
+	}
+}
---
+++ Rarities/Rarities/UpgradedRarity.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.Xna.Framework;
+using Terraria.ModLoader;
+
+namespace ANB.Rarities
+{
+	public class UpgradedRarity : ModRarity
+	{
+		public override Color RarityColor => new Color(128, 0, 128);
+	}
+}
