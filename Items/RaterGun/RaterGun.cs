de6b31ff3eedca235b088f1fd5b32ed7bfc39dcf
KIRILLAND <55920722+KIRILLAND78@users.noreply.github.com>
2022-04-07 23:39:02 +0300

    implemented acid gun

---
+++ Items/RaterGun/RaterGun.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Terraria.ModLoader;
+using Terraria.ID;
+using Terraria;
+using Microsoft.Xna.Framework;
+using ANB.Projectiles.RaterGunProj;
+using Terraria.DataStructures;
+
+namespace ANB.Items.RaterGun
+{
+    internal class RaterGun : ModItem
+    {
+
+        public override void SetStaticDefaults()
+        {
+            Tooltip.SetDefault("Uses gel as ammo\r\n33% chance to not consume ammo");
+            DisplayName.SetDefault("Acid Eater Gun");
+            base.SetStaticDefaults();
+        }
+
+        public override void SetDefaults()
+        {
+            Item.autoReuse = true;
+            Item.shootSpeed = 12;
+            Item.useTime = 8;
+            Item.useAnimation = 8;
+            
+            Item.useStyle = ItemUseStyleID.Shoot;
+            Item.useAmmo = ItemID.Gel;
+            Item.DamageType = DamageClass.Ranged;
+            Item.damage = 8;
+            Item.shoot = ModContent.ProjectileType<RaterGunProj>();
+            base.SetDefaults();
+        }
+        public override Vector2? HoldoutOffset()
+        {
+            return new Vector2(5,2);
+        }
+        public override bool CanConsumeAmmo(Player player)
+        {
+
+            if (Main.rand.NextFloat(1) <= 0.33f)
+            {
+                return false;
+            }
+            return base.CanConsumeAmmo(player);
+        }
+        public override bool Shoot(Player player, EntitySource_ItemUse_WithAmmo source, Vector2 position, Vector2 velocity, int type, int damage, float knockback)
+        {
+            Projectile.NewProjectile(source, position,velocity.RotatedByRandom(MathHelper.ToRadians(10)), type, damage, knockback, player.whoAmI);
+            velocity=velocity.RotatedByRandom(MathHelper.ToRadians(10));
+            return base.Shoot(player, source, position, velocity, type, damage, knockback);
+        }
+    }
+}
---
+++ Projectiles/RaterGunProj/RaterGunProj.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Terraria.ModLoader;
+using Terraria.ID;
+using Terraria;
+
+namespace ANB.Projectiles.RaterGunProj
+{
+    
+    internal class RaterGunProj : ModProjectile
+    {
+
+        public override void SetDefaults()
+        {
+            Projectile.extraUpdates = 2;
+            Projectile.timeLeft = 40;
+            Projectile.friendly = true;
+            Projectile.hostile = false;
+            Projectile.width = 2;
+            Projectile.height = 2;
+            
+            base.SetDefaults();
+        }
+        public override void OnHitNPC(NPC target, int damage, float knockback, bool crit)
+        {
+            target.AddBuff(BuffID.CursedInferno, 30);
+            base.OnHitNPC(target, damage, knockback, crit);
+        }
+        public override void OnHitPlayer(Player target, int damage, bool crit)
+        {
+            target.AddBuff(BuffID.CursedInferno, 30);
+            base.OnHitPlayer(target, damage, crit);
+        }
+        public override void OnHitPvp(Player target, int damage, bool crit)
+        {
+            target.AddBuff(BuffID.CursedInferno, 30);
+            base.OnHitPvp(target, damage, crit);
+        }
+
+        public override void AI()
+        {
+            Projectile.velocity.Y += 0.14f;
+            if (Projectile.timeLeft <= 35)
+            {
+                Dust.NewDust(Projectile.position, 1, 1, DustID.CursedTorch);
+            }
+            base.AI();
+        }
+
+    }
+}
