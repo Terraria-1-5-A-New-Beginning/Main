2ab35e17a13e9b069cf2a2caac6d5b305ce3bb06
ikee1 <isaachreid@gmail.com>
2022-04-12 23:00:52 +0100

    Added initial things for starsword will do more tmrw

---
+++ Items/StarSword/StarSword.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Terraria.ModLoader;
+using Terraria.ID;
+using Terraria;
+using Terraria.DataStructures;
+using Microsoft.Xna.Framework;
+
+namespace ANB.Items.StarSword
+{
+    public class StarSword : ModItem
+    {
+        public override void SetStaticDefaults()
+        {
+            DisplayName.SetDefault("Comet");
+            Tooltip.SetDefault("Summons stars which chase enemies");
+            base.SetStaticDefaults();
+        }
+
+        public override void SetDefaults()
+        {
+            Item.height = 14; //placeholder
+            Item.width = 14; //placeholder
+            Item.damage = 20; //depends where in game
+            Item.shoot = ModContent.ProjectileType<Projectiles.StarSwordProj.Star>();
+            Item.useTime = 20; //change
+            Item.useAnimation = 20; //change
+            Item.knockBack = 4;
+            Item.useStyle = ItemUseStyleID.Swing;
+            Item.value = 300;
+            base.SetDefaults();
+        }
+    }
+}
---
+++ Projectiles/StarSwordProj/Star.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Terraria.ModLoader;
+using Terraria.ID;
+using Terraria;
+using Terraria.DataStructures;
+using Microsoft.Xna.Framework;
+
+namespace ANB.Projectiles.StarSwordProj
+{
+    public class Star : ModProjectile
+    {
+        public override void SetDefaults()
+        {
+            Projectile.height = 6; //placeholder
+            Projectile.width = 6; //placeholder
+        }
+    }
+}
