efe61281e035d409181a2dd0b48437fb6b0c5165
ikee1 <isaachreid@gmail.com>
2022-07-13 22:47:40 +0100

    mirror

---
+++ Items/GiantMirror.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.Xna.Framework;
+using Terraria;
+using Terraria.ID;
+using Terraria.GameContent.Creative;
+using Terraria.ModLoader;
+using Terraria.DataStructures;
+using ANB.Projectiles;
+
+namespace ANB.Items
+{
+    public class GiantMirror : ModItem
+    {
+        public override void SetStaticDefaults()
+        {
+            Tooltip.SetDefault("A giant mirror\nWhen you hit an enemy, rays of light will explode out in all directions");
+
+            CreativeItemSacrificesCatalog.Instance.SacrificeCountNeededByItemId[Type] = 1;
+        }
+        public override void SetDefaults()
+        {
+            Item.damage = 20;
+            Item.height = 20;
+            Item.width = 20;//everything above is placeholder
+            Item.useStyle = ItemUseStyleID.Swing;
+            Item.useTime = 20;
+            Item.useAnimation = 20;
+            Item.autoReuse = true;
+            Item.DamageType = DamageClass.Melee;
+            Item.knockBack = 3;
+            Item.crit = 12;
+            Item.rare = ItemRarityID.Green;
+            Item.UseSound = SoundID.Item1;
+        }
+        public override void OnHitNPC(Player player, NPC target, int damage, float knockBack, bool crit)
+        {
+
+            base.OnHitNPC(player, target, damage, knockBack, crit);
+            Vector2 pos = target.Center;
+            Vector2 velocity = Vector2.Normalize(player.position - target.position).RotatedByRandom(3.14159);
+            IEntitySource source = new EntitySource_OnHit(player, target);
+            Projectile.NewProjectile(source, pos, velocity, ModContent.ProjectileType<LightRay>(), damage, knockBack, Main.myPlayer);
+        }
+    }
+}
---
+++ Projectiles/LightRay.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.Xna.Framework;
+using Terraria;
+using Terraria.ID;
+using Terraria.GameContent.Creative;
+using Terraria.ModLoader;
+using Terraria.DataStructures;
+using ANB.Items;
+
+namespace ANB.Projectiles
+{
+    public class LightRay : ModProjectile
+    {
+        public override void SetDefaults()
+        {
+            Projectile.damage = 20;
+            Projectile.height = 20;
+            Projectile.width = 20;
+            Projectile.DamageType = DamageClass.Melee;
+            Projectile.knockBack = 0.5f;
+            Projectile.aiStyle = 36;
+        }
+    }
+}
